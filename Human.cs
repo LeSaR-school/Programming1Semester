df168a1906e31a3e66731e30a23a5949839acc89
LeSaR <ilkov2006@gmail.com>
2022-12-24 12:02:40 +0300

    Initial commit

---
+++ Human.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Human{
+
+	public string Name { get; private set; } = "Lyceum Student";
+
+	private int _age = 0;
+	public int Age{
+		get => _age;
+		set{
+			if(value >= 0) _age = value;
+		}
+	}
+
+	private float _weight = 0f;
+	public float WeightKG{
+		get => _weight;
+		set{
+			if(value >= 0f) _weight = value;
+		}
+	}
+
+	public Human(){}
+
+	public Human(string name){
+
+		Name = name;
+
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Human h1 = new Human();
+Human h2 = new Human("Ilya");
+
+Console.WriteLine(h1.Name);
+Console.WriteLine(h2.Name);
